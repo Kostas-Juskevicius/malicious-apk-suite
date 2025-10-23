a93ce647c1adb95aeb48289d39e8e74e07dc41b8
Kostas-Juskevicius <kostas.jusk@gmail.com>
2025-10-23 16:08:20 +0300

    Strings dict bug in grepping

--- static/python/ripgrep.py
+++ static/python/ripgrep.py
@@ -155,7 +155,7 @@ STRINGS_PATTERNS = [
     r"wss://",
     r"ftp://",
     r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",  # IP addresses
-    r"[a-zA-Z0-9-]+\.(com|net|org|io|xyz|ru|cn|onion)",  # domains
+    r"[a-zA-Z0-9-]+\.(com|net|org|io|xyz|ru|cn|onion)\b",  # domains
 ]
 
 PERSISTENCE_PATTERNS = [
--- static/python/strings.py
+++ static/python/strings.py
@@ -19,7 +19,7 @@ STRINGS_PATTERNS = [
     r"wss://",
     r"ftp://",
     r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",  # IP addresses
-    r"[a-zA-Z0-9-]+\.(com|net|org|io|xyz|ru|cn|onion)",  # domains
+    r"[a-zA-Z0-9-]+\.(com|net|org|io|xyz|ru|cn|onion)\b",  # domains
     r"aHR0cDovL2",  # base64 http://
     r"aHR0cHM6Ly9",  # base64 https://
 ]
@@ -50,7 +50,7 @@ for res_file in resource_files:
         
         for match in grep_result.stdout.strip().split('\n'):
             if match:
-                print(f"[*] FOUND STRING: {match} in {res_file}")
+                print(f"[*] FOUND STRING  / PATTERN: '{match}' in {res_file}")
     print()
 
 print("[*] EXTRACTION COMPLETE")
