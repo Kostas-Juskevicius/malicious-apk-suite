5f42a6984d82aa0d4bd82875f3b86859fe031afb
Kostas-Juskevicius <kostas.jusk@gmail.com>
2025-10-27 12:56:14 +0200

    Removed redundant folder

--- dynamic/template.js
+++
@@ -1,9 +0,0 @@
-Java.perform(function () {
-  var classObj = Java.use("");
-
-  try {
-    classObj.methodName.implementation = function (arg1) {
-      console.log("\n[*] ");
-    };
-  } catch (e) {}
-});
