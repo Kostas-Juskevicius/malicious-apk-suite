28557c695ec31cdf4f83036f495270f023211bab
Kostas-Juskevicius <kostas.jusk@gmail.com>
2025-10-27 10:57:45 +0200

    Added component log skipping functionality in case they come from legit packages

--- static/python/manifest_analysis.py
+++ static/python/manifest_analysis.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 from lxml import etree
 
+IGNORABLE_COMPONENTS = [
+    "com.google.android.gms",
+    "com.google.firebase",
+    "com.google.android.datatransport",
+]
 
 DANGEROUS_PERMISSIONS = [
     "android.permission.ACCEPT_HANDOVER",
@@ -65,7 +70,7 @@ for permission in DANGEROUS_PERMISSIONS:
 print()
 
 
-# Log components
+# Component logging helpers
 def qualify_name(name: str) -> str:
     """
     If name starts with '.', qualify it with package.
@@ -74,32 +79,41 @@ def qualify_name(name: str) -> str:
         return f"{package}{name}"
     return name
 
+def is_ignorable(component_name: str) -> bool:
+    return any(component_name.startswith(prefix) for prefix in IGNORABLE_COMPONENTS)
+
+
+# Log components
 application = root.xpath("//application/@android:name", namespaces=ns)
-print(f"[*] FOUND: application: {qualify_name(application[0])}\n")
+if not is_ignorable(qualify_name(application[0])):
+    print(f"[*] FOUND: application: {qualify_name(application[0])}\n")
 
 main_activity = root.xpath(
     "//activity[intent-filter/action[@android:name='android.intent.action.MAIN'] and intent-filter/category[@android:name='android.intent.category.LAUNCHER']]/@android:name",
     namespaces=ns
 )
-if main_activity:
-    print(f"[*] FOUND: main activity that's a launcher (appears on home screen): {qualify_name(main_activity)}\n")
+if main_activity and not is_ignorable(qualify_name(main_activity[0])):
+    print(f"[*] FOUND: main activity that's a launcher (appears on home screen): {qualify_name(main_activity[0])}\n")
 
 for activity in root.xpath("//activity/@android:name", namespaces=ns):
-    print(f"[*] FOUND: activity: {qualify_name(activity)}")
+    if not is_ignorable(qualify_name(activity)):
+        print(f"[*] FOUND: activity: {qualify_name(activity)}")
 print()
 
 for service in root.xpath("//service/@android:name", namespaces=ns):
-    print(f"[*] FOUND: service: {qualify_name(service)}")
+    if not is_ignorable(qualify_name(service)):
+        print(f"[*] FOUND: service: {qualify_name(service)}")
 print()
 
 for receiver in root.xpath("//receiver"):
-    receiver_name = receiver.xpath("@android:name", namespaces=ns)
-    r_name = receiver_name[0] if receiver_name else "Unknown"
-    print(f"[*] FOUND: receiver: {qualify_name(r_name)}")
-    actions = receiver.xpath("intent-filter/action/@android:name", namespaces=ns)
-    for action in actions:
-        print(f"\t[*] FOUND: receiver action: {action}")
+    receiver_name = receiver.xpath("@android:name", namespaces=ns)[0]
+    if not is_ignorable(qualify_name(receiver_name)):
+        print(f"[*] FOUND: receiver: {qualify_name(receiver_name)}")
+        actions = receiver.xpath("intent-filter/action/@android:name", namespaces=ns)
+        for action in actions:
+            print(f"\t[*] FOUND: receiver action: {action}")
 print()
 
 for provider in root.xpath("//provider/@android:name", namespaces=ns):
-    print(f"[*] FOUND: provider: {qualify_name(provider)}")
+    if not is_ignorable(qualify_name(provider)):
+        print(f"[*] FOUND: provider: {qualify_name(provider)}")
